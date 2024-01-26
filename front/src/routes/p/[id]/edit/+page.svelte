20d7dbab789e76d0cec1be3323509f83c45591ab
jhs512 <jangka512@gmail.com>
2024-01-26 10:58:09 +0900

    work

--- front/src/routes/p/[id]/edit/+page.svelte
+++ front/src/routes/p/[id]/edit/+page.svelte
@@ -1,10 +1,80 @@
 <script lang="ts">
   import { page } from '$app/stores';
   import rq from '$lib/rq/rq.svelte';
+  import hotkeys from 'hotkeys-js';
 
   import ToastUiEditor from '$lib/components/ToastUiEditor.svelte';
 
   let toastUiEditor: any | undefined;
+  let oldBody: string = '';
+
+  function saveToLocalStorage(id: number, body: string) {
+    const key = 'posts_' + id;
+    // LocalStorage에서 기존 데이터를 가져옵니다.
+    const existingData = localStorage.getItem(key);
+
+    // 기존 데이터가 있으면 JSON으로 파싱하고, 없으면 빈 배열을 사용합니다.
+    const posts = existingData ? JSON.parse(existingData) : [];
+
+    // 새 데이터를 배열에 추가합니다.
+    posts.push({
+      id,
+      body: body,
+      date: new Date().toISOString()
+    });
+
+    // 배열의 크기가 10을 초과하면 가장 오래된 항목(첫 번째 항목)을 제거합니다.
+    if (posts.length > 10) {
+      posts.shift(); // 배열의 첫 번째 항목을 제거합니다.
+    }
+
+    // 변경된 배열을 JSON 문자열로 변환하여 LocalStorage에 저장합니다.
+    localStorage.setItem(key, JSON.stringify(posts));
+  }
+
+  async function Post__saveBody() {
+    const newBody = toastUiEditor.editor.getMarkdown().trim();
+
+    if (oldBody === newBody) {
+      return;
+    }
+
+    const { data, error } = await rq.apiEndPoints().PUT('/api/v1/posts/{id}/body', {
+      params: { path: { id: parseInt($page.params.id) } },
+      body: { body: newBody }
+    });
+
+    oldBody = newBody;
+
+    saveToLocalStorage(parseInt($page.params.id), newBody);
+
+    if (data) {
+      rq.msgInfo(data.msg);
+    }
+  }
+
+  rq.effect(() => {
+    hotkeys.filter = function (event) {
+      return true;
+    };
+
+    hotkeys('ctrl+s,cmd+s', 'postEdit', function (event, handler) {
+      Post__saveBody();
+
+      event.preventDefault();
+    });
+
+    hotkeys('ctrl+q,cmd+q', 'postEdit', function (event, handler) {
+      toastUiEditor.switchTab();
+      event.preventDefault();
+    });
+
+    hotkeys.setScope('postEdit');
+
+    return () => {
+      hotkeys.deleteScope('postEdit');
+    };
+  });
 
   async function load() {
     if (import.meta.env.SSR) throw new Error('CSR ONLY');
@@ -77,7 +147,7 @@
     <div>
       <div>내용</div>
       {#key post.id}
-        <ToastUiEditor bind:this={toastUiEditor} body={post.body} />
+        <ToastUiEditor bind:this={toastUiEditor} body={post.body} saveBody={Post__saveBody} />
       {/key}
     </div>
 
