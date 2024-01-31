f1d802c2d89fa39ad896dc163e35394e5d32e03d
jhs512 <jangka512@gmail.com>
2024-01-31 09:39:17 +0900

    work

--- front/src/routes/+page.svelte
+++ front/src/routes/+page.svelte
@@ -1,9 +1,24 @@
-<div>메인 입니다.</div>
+<div class="flex-grow flex justify-center items-center">
+  <div>
+    <div class="text-center">
+      <div class="font-bold text-lg">SLOG, 개발자를 위한 블로그</div>
+      <div class="mt-3 text-gray-400">
+        기술관련 글을 작성할 때 편리한 에디터와 뷰어를 제공합니다.
+      </div>
+    </div>
 
-<ul>
-  <li>기존 회원분들 걱정마세요.</li>
-  <li>모든 글 살아 있습니다.</li>
-  <li>일반 로그인은 사라졌습니다.</li>
-  <li>소셜 로그인만 가능합니다.</li>
-  <li>소셜 로그인 후 상단 메뉴에 보면 '기존 글 가져오기' 불러오기가 있습니다.</li>
-</ul>
+    <div class="divider"></div>
+
+    <div>
+      <div class="font-bold text-lg text-center">공지사항</div>
+
+      <ul class="list-disc m-5 text-gray-400">
+        <li>wiken.io, scode.gg 와 통합되었습니다.</li>
+        <li>wiken.io, scode.gg 의 모든 글들에 대한 접근은 여전히 가능합니다.</li>
+        <li>로그인 후 상단 메뉴에서 '기존 글 가져오기' 를 볼 수 있습니다.</li>
+        <li>일반 로그인 기능은 더 이상 제공되지 않습니다.</li>
+        <li>소셜 로그인만 가능합니다.</li>
+      </ul>
+    </div>
+  </div>
+</div>
