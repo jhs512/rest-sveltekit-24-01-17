a12255a4d713fd628b9791ad5b9582aafdb9a73c
jhs512 <jangka512@gmail.com>
2024-02-01 11:46:30 +0900

    work

--- front/src/routes/+page.svelte
+++ front/src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <div class="flex-grow flex justify-center items-center">
-  <div class="w-full max-w-sm">
-    <div class="text-center">
+  <div class="w-full max-w-lg">
+    <div class="text-center mt-4">
       <div class="font-bold text-lg">SLOG, 개발자를 위한 블로그</div>
       <div class="mt-3 text-gray-400">
         기술관련 글을 작성할 때 편리한 에디터와 뷰어를 제공합니다.
@@ -12,12 +12,39 @@
     <div>
       <div class="font-bold text-lg text-center">공지사항</div>
 
-      <ul class="list-disc m-5 text-gray-400">
-        <li>wiken.io, scode.gg 와 통합되었습니다.</li>
-        <li>wiken.io, scode.gg 의 모든 글들에 대한 접근은 여전히 가능합니다.</li>
-        <li>로그인 후 상단 메뉴에서 '기존 글 복구' 를 볼 수 있습니다.</li>
-        <li>일반 로그인 기능은 더 이상 제공되지 않습니다.</li>
-        <li>소셜 로그인만 가능합니다.</li>
+      <ul class="list-disc my-4 ml-7 mr-2 text-gray-400">
+        <li>
+          <span class="text-red-500">Ctrl + D</span>(<span class="text-red-500">Cmd + D</span>)는 글
+          본문페이지에서 <span class="text-red-500">본문 풀스크린 모드</span>를 토글합니다.
+          <i class="fa-solid fa-fire text-red-500"></i>
+        </li>
+        <li>
+          <span class="text-red-500">Ctrl + P</span>(<span class="text-red-500">Cmd + P</span>)는
+          에디터에서 <span class="text-red-500">미리보기 모드</span>를 토글합니다.
+          <i class="fa-solid fa-fire text-red-500"></i>
+        </li>
+        <li>
+          <span class="text-red-500">Ctrl + S</span>(<span class="text-red-500">Cmd + S</span>)는
+          에디터에서 <span class="text-red-500">본문(글 혹은 댓글)을 저장</span>합니다.
+          <i class="fa-solid fa-fire text-red-500"></i>
+        </li>
+        <li>
+          <span class="text-red-500">Ctrl + Q</span>(<span class="text-red-500">Cmd + Q</span>)는
+          <span class="text-red-500">에디터 풀스크린 모드</span>를 토글합니다.
+          <i class="fa-solid fa-fire text-red-500"></i>
+        </li>
+        <li>
+          <span class="text-red-500">본문 풀스크린 모드</span>에서는 문서가 수정되었을 때 즉시 해당
+          <span class="text-red-500">변경사항이 반영</span>되어 보여집니다.
+          <i class="fa-solid fa-fire text-red-500"></i>
+        </li>
+        <li>
+          로그인 후 상단 메뉴에서 <span class="text-red-500">기존 글 복구</span> 를 볼 수 있습니다.
+        </li>
+        <li>
+          일반 로그인 기능은 더 이상 제공되지 않습니다. <span class="text-red-500">소셜로그인</span
+          >만 가능합니다.
+        </li>
       </ul>
     </div>
   </div>
